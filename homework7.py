539ecf81e0f42b6709f5c6912c193455302c68ab
flygunsoo <geonsoo@uw.edu>
2016-03-02 14:25:28 -0800

    adding
    hi

---
+++ homework7.py
@@ -0,0 +1,8 @@
+# Name: Jongyun Park | Geon Soo Park
+# UW NetID: 1469063 | 1260164
+# Section: AC | AB
+# CSE 160
+# Homework 7: Analyzing Smoking Rate from Effectiveness of  \
+#             Increasing Price in Tobacco (Korea Specialized)
+
+
